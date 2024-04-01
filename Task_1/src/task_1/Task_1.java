019f97be04caf7f5a20d79c0433c1feef0aa01d4
Roma-Cherny-15 <145929935+Roma-Cherny-15@users.noreply.github.com>
2024-04-01 23:27:16 +0300

    Task_1 completed

---
+++ Task_1/src/task_1/Task_1.java
@@ -0,0 +1,17 @@
+
+package task_1;
+
+public class Task_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Завдання 1");
+        System.out.println("Аргументи передані через командну строку: ");
+        for(String arg: args) {
+            System.out.println(arg);
+        }
+    }
+    
+}
